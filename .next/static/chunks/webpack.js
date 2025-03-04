5c8094ce3222b5ef040db4b5abc62fb5373e3de9
Austen Tucker <austen@thearcades.me>
2025-03-04 12:47:03 -0600

    Refactor NavBar component: Simplified menu structure and improved markup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b551f6ce2851c939"; }
+/******/ 		__webpack_require__.h = function() { return "adcf1f9873b8594c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
