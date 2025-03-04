864cd1b66591dc5dc0f1e8b2e471f8a8052119d8
Austen Tucker <austen@thearcades.me>
2025-03-04 14:29:32 -0600

    Configure Vercel deployment and update resume link filename

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -25,7 +25,7 @@ export default function NavBar() {
         <ul className="button-grid">
           <li><a className="text-link" href="/" onMouseEnter={(e) => e.target.classList.add('gaysparkles')} onMouseLeave={(e) => e.target.classList.remove('gaysparkles')}>Home</a></li>
           <li><a className="text-link" href="/books" onMouseEnter={(e) => e.target.classList.add('gaysparkles')} onMouseLeave={(e) => e.target.classList.remove('gaysparkles')}>Books</a></li>
-          <li><a className="text-link" href="/resume/Tucker Austen Scrum Master.pdf" onMouseEnter={(e) => e.target.classList.add('gaysparkles')} onMouseLeave={(e) => e.target.classList.remove('gaysparkles')}>Resume</a></li>
+          <li><a className="text-link" href="/resume/TuckerAustenScrumMaster.pdf" onMouseEnter={(e) => e.target.classList.add('gaysparkles')} onMouseLeave={(e) => e.target.classList.remove('gaysparkles')}>Resume</a></li>
           <li><a className="text-link" href="https://www.linkedin.com/in/austen-tucker-0968a914/" onMouseEnter={(e) => e.target.classList.add('gaysparkles')} onMouseLeave={(e) => e.target.classList.remove('gaysparkles')}>LinkedIn</a></li>
           <li><a className="text-link" href="/did" onMouseEnter={(e) => e.target.classList.add('gaysparkles')} onMouseLeave={(e) => e.target.classList.remove('gaysparkles')}>Why the Arcades?</a></li>
           <li><a className="text-link" href="/projects" onMouseEnter={(e) => e.target.classList.add('gaysparkles')} onMouseLeave={(e) => e.target.classList.remove('gaysparkles')}>Projects</a></li>
--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,6 @@ const IntroContent = () => (
             </tbody>
           </table>
         </div>
-        
       </div>
     </div>
   </div>
